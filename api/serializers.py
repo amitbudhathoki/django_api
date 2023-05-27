59d6d6bc64a6839499e12b57fa564c2b6e870085
Amit Budhathoki <amitbudhathoki30@gmail.com>
2023-05-27 22:32:59 +0545

    company and employee api completed

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import Company
+from api.models import Company,Employee
 
 
 #create serializers here
@@ -9,3 +9,8 @@ class CompanySerializer(serializers.HyperlinkedModelSerializer):
         model=Company
         fields = "__all__"
 
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+    employee_id = serializers.ReadOnlyField()
+    class Meta:
+        model = Employee
+        fields = "__all__"
